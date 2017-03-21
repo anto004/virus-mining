f9ec397d5c3b6ab126d1357675754a82d4f50462
Antonio Bang <anto_004@ymail.com>
2017-03-20 17:04:22 -0700

    package added

---
+++ src/main/java/Acquisition/App.java
@@ -0,0 +1,29 @@
+package Acquisition;
+
+import com.mongodb.MongoClient;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+
+public class App {
+    private static void importCSV(){
+
+    }
+
+    public static void main(String[] args){
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("zika");
+        MongoCollection<Document> collection = mongoDatabase.getCollection("Argentina");
+
+        MongoCursor<Document> cursor = collection.find().iterator();
+
+        while(cursor.hasNext()){
+            System.out.println(cursor.next().toJson());
+        }
+
+
+    }
+}
